537e07b56855e7940c0b2eeb73b12213f4389abc
sunwise <sunwise9634@gmail.com>
2023-06-20 22:59:04 +0900

    이벤트 추가하기

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ function Header(props){
   console.log('props', props.title)
   return (
     <header>
-      <h1><a href='/'>{props.title}</a></h1>
+      <h1><a href='/' onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
   )
 }
@@ -11,7 +14,11 @@ function Nav(props){
   const lis = []
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id); // event.target 은 <a> 태그를 가리킴
+    }}>{t.title}</a></li>)
   }
   return (
     <nav>
@@ -37,8 +44,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header')
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id)
+      }}></Nav>
       <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
